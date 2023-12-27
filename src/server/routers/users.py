d8f609c488923692cd6066e331378af8e81db481
puip34 <123201605+puip34@users.noreply.github.com>
2023-12-27 11:50:07 +0300

    Update users.py

--- src/server/routers/users.py
+++ src/server/routers/users.py
@@ -1,13 +1,8 @@
-import fastapi
-from drug_store.src.server.database.models import Users
-from drug_store.src.server.resolvers import users
+from fastapi import APIRouter, Depends
+from drug_store.src.server.auth.auth_utils import get_current_user
 
-users_router = fastapi.APIRouter(prefix='/users', tags=["Users"])
+router = APIRouter(prefix='/users', tags=["Users"])
 
-@users_router.post(path='/login', response_model=dict)
-def login(username: str, password: str) -> dict:
-    return users.login(username, password)
-
-@users_router.put(path='/change_password/{user_id}', response_model=dict)
-def change_password(user_id: int, new_password: str) -> dict:
-    return users.change_password(user_id, new_password)
+@router.get("/me", response_model=dict)
+async def read_users_me(current_user: dict = Depends(get_current_user)):
+    return current_user
