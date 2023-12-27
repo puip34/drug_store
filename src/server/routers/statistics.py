c15a3bc0768a4258a195c9aa3c2f4e8d0bc9ad23
puip34 <123201605+puip34@users.noreply.github.com>
2023-12-27 10:40:52 +0300

    Update statistics.py

--- src/server/routers/statistics.py
+++ src/server/routers/statistics.py
@@ -1,14 +1,8 @@
-from drug_store.src.server.database.db_manager import db_manager
+import fastapi
+from drug_store.src.server.resolvers import statistics
 
+statistics_router = fastapi.APIRouter(prefix='/statistics', tags=["Statistics"])
+
+@statistics_router.get(path='/calculate_performance', response_model=dict)
 def calculate_performance_statistics() -> dict:
-    res = db_manager.execute(
-        query="""SELECT COUNT(*) AS num_completed_applications,
-                        AVG(DATEDIFF(completed_date, date_added)) AS avg_time_to_complete,
-                        drug_id,
-                        COUNT(DISTINCT drug_id) AS num_drug_types
-                  FROM Applications
-                  WHERE status = 'completed'
-                  GROUP BY drug_id"""
-    )
-
-    return res
+    return statistics.calculate_performance_statistics()
