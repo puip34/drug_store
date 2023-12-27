9b1136ede30c6ca6fad99227a34e313d6759d4ca
puip34 <123201605+puip34@users.noreply.github.com>
2023-12-27 11:33:48 +0300

    Update db_manager.py

--- src/server/database/db_manager.py
+++ src/server/database/db_manager.py
@@ -1,20 +1,10 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
-from contextlib import contextmanager
 
-# Database URL (replace 'your_database_url' with the actual database URL)
-DATABASE_URL = "your_database_url"
-
-# Create a database engine
+DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL)
 
-# Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-@contextmanager
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+Base: DeclarativeMeta = declarative_base()
