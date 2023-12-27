233ca88b88bd1334cd6e6b7da1db8a698951228e
puip34 <123201605+puip34@users.noreply.github.com>
2023-12-27 11:50:55 +0300

    Update applications.py

--- src/server/routers/applications.py
+++ src/server/routers/applications.py
@@ -1,21 +1,13 @@
-import fastapi
-from drug_store.src.server.database.models import Applications
-from drug_store.src.server.resolvers import applications
+from fastapi import APIRouter, Depends
+from drug_store.src.server.auth.auth_utils import get_current_user
 
-applications_router = fastapi.APIRouter(prefix='/applications', tags=["Applications"])
+router = APIRouter(prefix='/applications', tags=["Applications"])
 
-@applications_router.post(path='/add', response_model=dict)
-def add_application(application: Applications) -> dict:
-    return applications.add_application(application)
-
-@applications_router.put(path='/edit/{application_id}', response_model=dict)
-def edit_application(application_id: int, new_status: str, new_drug_id: int) -> dict:
-    return applications.edit_application(application_id, new_status, new_drug_id)
-
-@applications_router.get(path='/track/{application_id}', response_model=dict)
-def track_application_status(application_id: int) -> dict:
-    return applications.track_application_status(application_id)
-
-@applications_router.put(path='/assign/{application_id}', response_model=dict)
-def assign_responsible(application_id: int, seller_id: int, comment: str) -> dict:
-    return applications.assign_responsible(application_id, seller_id, comment)
+@router.post("/add", response_model=dict)
+async def add_application(
+    drug: str,
+    customer: str,
+    current_user: dict = Depends(get_current_user)
+):
+    
+    return {"message": "Application added successfully"}
