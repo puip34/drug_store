335419eeb9bb5d4093c7b8072e3cdca17996530e
puip34 <123201605+puip34@users.noreply.github.com>
2023-12-27 11:47:40 +0300

    Update auth_utils.py

--- src/server/auth/auth_utils.py
+++ src/server/auth/auth_utils.py
@@ -1,9 +1,10 @@
-from fastapi import HTTPException, Depends
+from fastapi import HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
-    # Your authentication logic here
-    # For example, decode the token and fetch user information
-    return {"sub": token}
+    # Placeholder logic: Validate token and fetch user
+    # For now, let's assume we return a dummy user
+    user = {"username": "john_doe", "role": "admin"}
+    return user
