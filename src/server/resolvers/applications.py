72458e1b6003805ad7bcee5424957ec288172da0
puip34 <123201605+puip34@users.noreply.github.com>
2023-12-27 11:37:00 +0300

    Update applications.py

--- src/server/resolvers/applications.py
+++ src/server/resolvers/applications.py
@@ -1,45 +1,15 @@
-from drug_store.src.server.database.db_manager import db_manager
-from drug_store.src.server.database.models import Applications
+from fastapi import APIRouter
+from drug_store.src.server.database.models import Application
+from drug_store.src.server.database.db_manager import SessionLocal
 
-def add_application(application: Applications) -> dict:
-    res = db_manager.execute(
-        query="""INSERT INTO Applications (application_number, date_added, drug_id, customer_data, status)
-                  VALUES (?, ?, ?, ?, ?)
-                  RETURNING id""",
-        args=(application.application_number, application.date_added, application.drug_id,
-              application.customer_data, application.status)
-    )
+applications_router = APIRouter()
 
-    return res
-
-def edit_application(application_id: int, new_status: str, new_drug_id: int) -> dict:
-    res = db_manager.execute(
-        query="""UPDATE Applications
-                  SET status = ?, drug_id = ?
-                  WHERE id = ?
-                  RETURNING id""",
-        args=(new_status, new_drug_id, application_id)
-    )
-
-    return res
-
-def track_application_status(application_id: int) -> dict:
-    res = db_manager.execute(
-        query="""SELECT id, application_number, date_added, drug_id, customer_data, status
-                  FROM Applications
-                  WHERE id = ?""",
-        args=(application_id,)
-    )
-
-    return res
-
-def assign_responsible(application_id: int, seller_id: int, comment: str) -> dict:
-    res = db_manager.execute(
-        query="""UPDATE Applications
-                  SET seller_id = ?, comment = ?
-                  WHERE id = ?
-                  RETURNING id""",
-        args=(seller_id, comment, application_id)
-    )
-
-    return res
+@applications_router.post("/add_application")
+async def add_application(application: Application):
+    # Your logic to add an application
+    db = SessionLocal()
+    db.add(application)
+    db.commit()
+    db.refresh(application)
+    db.close()
+    return {"message": "Application added successfully"}
